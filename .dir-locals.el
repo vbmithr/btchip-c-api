c97620e3a7fdafb621f7e87e6c0c193f668dba0d
Vincent Bernardoff <vb@luminar.eu.org>
2017-05-10 22:53:26 +0200

    add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 4)
+         (fill-column . 80)))
+ ;; Warn about spaces used for indentation:
+ (haskell-mode . ((eval . (highlight-regexp "^ *"))))
+ (c-mode . ((c-file-style . "BSD")))
+ (java-mode . ((c-file-style . "BSD")))
+ ("src/imported"
+  . ((nil . ((change-log-default-name . "ChangeLog.local"))))))
